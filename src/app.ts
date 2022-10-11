47c262b1b196148b32d850c4652f10be9119266b
ChristoferMendes <christoferluizdsm@gmail.com>
2022-10-11 09:19:48 -0300

    added consign and bodyParser

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello World!');
